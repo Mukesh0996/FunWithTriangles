5c90655918e376b54785c1528b6101fe98019c02
Mukesh <mukeshkeerthipati96@gmail.com>
2021-07-21 23:25:51 +0530

    refractored JS file

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const submitBtn = document.querySelector("button[type='submit']");
-const form = document.forms[0];
+const inputs = document.querySelectorAll('input[type="number"]')
+const output = document.querySelector(".output");
+let angles = [], sum = 0;
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    const formData = new FormData(form);
-    for(let fields of formData) {
-        console.log(fields);
+    inputs.forEach(i=> {
+        angles.push(Number(i.value));
+    })
+    // calculating sum
+    sum = angles.reduce((value, currentNumber) => value + currentNumber ,0);
+  
+    if(sum === 0 ) {
+        return;
+    }
+     else if(sum === 180) {
+        output.innerText = "Wohoo! You can make a triangle with these set of angles.."
+    } else {
+        output.innerText = "Oops! You cannot make a triangle with these set of angles..";
     }
 });
